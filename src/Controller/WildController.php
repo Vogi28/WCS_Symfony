216ebff8b2226b304e4562a27a987583d887ecf8
NGUYEN Vo Vinh Nghi <vogi.nguyen@live.fr>
2019-11-24 16:01:50 +0100

    symfony advanced routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/Wild/")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/Wild", name="wild_index")
+     * @Route("index", name="wild_index")
      */
     public function index() :Response
     {
@@ -17,4 +20,21 @@ class WildController extends AbstractController
             'website' => 'Wild Séries'
         ]);
     }
+
+    /**
+   * @Route("show/{slug<[a-z0-9\-]+>?}", name="wild_show")
+   */
+   public function show($slug): Response
+   {
+        if($slug !== null)
+        {
+            $slug = str_replace('-', ' ', $slug);
+            $slug = ucwords($slug,' ');
+        }
+        else
+        {
+            $slug = "Aucune série sélectionnée, veuillez choisir une série";    
+        }
+        return $this->render('Wild/show.html.twig', ['slug' => $slug]);
+   }
 }
